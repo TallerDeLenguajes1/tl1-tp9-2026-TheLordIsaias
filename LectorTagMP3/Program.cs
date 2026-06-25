cb37f093ca6ed344058e12238c78668810bcc243
TheLordIsaias <miruizselman@gmail.com>
2026-06-25 18:42:41 -0300

    TP Terminado

--- LectorTagMP3/Program.cs
+++ LectorTagMP3/Program.cs
@@ -2,7 +2,8 @@
 using System.IO;
 using System.Text;
 
-string directorio = "C:/Taller/tl1-tp9-2026-TheLordIsaias/LectorTagMP3/audio/fran-ruiz-pulse-333112.mp3";
+string directorio = "C:/Taller de Lenguajes/TP9/tl1-tp9-2026-TheLordIsaias/LectorTagMP3/audio/cancion2.mp3";
+
 
 FileInfo file = new FileInfo(directorio);
 
@@ -11,14 +12,37 @@ Console.WriteLine(file.Name);
 bool existe = File.Exists(directorio);
 
 byte[] buffer = new byte[128];
+if (File.Exists(directorio))
+{
+    FileStream archivo = new FileStream(directorio, FileMode.Open);
 
-FileStream archivo = new FileStream(directorio, FileMode.Open);
+    Encoding latin1 = Encoding.GetEncoding("latin1");
 
-Encoding latin1 = Encoding.GetEncoding("latin1");
+    archivo.Seek(-128, SeekOrigin.End);
+    archivo.Read(buffer, 0 , 128);
+
+    string identificador = latin1.GetString(buffer, 0, 3);
+
+    if(identificador == "TAG")
+    {
+        string tagCompleto = latin1.GetString(buffer, 0, 128);
+
+        string titulo = tagCompleto.Substring(3, 30);
+        string artista = tagCompleto.Substring(33, 30);
+        string album = tagCompleto.Substring(63, 30);
+        string anio = tagCompleto.Substring(93, 4);
+        Console.WriteLine($"Titulo: {titulo}. Artista: {artista}. Album: {album}. Año: {anio}");
+    } else
+    {
+        Console.WriteLine($"No se ha encontrado un TAG de ID3v1 en el archivo leido en {directorio}");
+    }  
+} else
+{
+    Console.WriteLine("No existe una cancion en el directorio proporcionado");
+}
 
 
 
-archivo.Seek(-128, SeekOrigin.End);
-archivo.Read(buffer, 0 , 128);
 
-Console.WriteLine(latin1.GetString(buffer, 0, 128));
+
+
